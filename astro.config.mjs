e35b7e67f5c679e8b8e1e1a3b1d2d86e573ef55d
salema97 <salemavelasquez97@gmail.com>
2024-03-19 03:40:06 -0500

    Deploy GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salema97.github.io',
-  base: '/',
   integrations: [tailwind()]
 });
