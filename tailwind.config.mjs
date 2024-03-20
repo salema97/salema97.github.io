955762752bd8ac6624d8f4e569a8dac03959a810
salema97 <salemavelasquez97@gmail.com>
2024-03-19 21:26:46 -0500

    Organize code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-	],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  tailwindConfig: "./styles/tailwind.config.js",
+  tailwindAttributes: ["myClassList"],
+  theme: {
+    extend: {},
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
