8b7a83af7d17e9db65d750237e79529f66013c5e
salema97 <salemavelasquez97@gmail.com>
2024-03-20 02:02:01 -0500

    Fix prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
-	],
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {}
 	},
