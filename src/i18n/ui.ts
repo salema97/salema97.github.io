76a93098cd8cfdb569b05c33e6644b51ae63f6aa
salema97 <salemavelasquez97@gmail.com>
2024-03-23 21:06:49 -0500

    Internationalization configuration

---
+++ src/i18n/ui.ts
@@ -0,0 +1,62 @@
+import SpainFlag from '../icons//SpainFlag.astro';
+import USFlag from '../icons/USFlag.astro';
+
+// Add missing imports
+export const LANGUAGES: Record<
+	string,
+	{ code: string; name: string; flag: typeof SpainFlag }
+> = {
+    es: {
+		code: 'es',
+		name: 'Español',
+		flag: SpainFlag,
+	},
+	en: {
+		code: 'en',
+		name: 'English',
+		flag: USFlag,
+	}
+};
+
+export const defaultLang = 'es';
+export const showDefaultLang = false;
+
+export const ui = {
+	es: {
+		'nav.inicio': 'Inicio',
+		'nav.vota': 'Vota',
+		'nav.info': 'Información',
+		'nav.archivo': 'Archivo',
+		'nav.legal': 'Aviso Legal',
+		'nav.privacidad': 'Privacidad',
+		'nav.cookies': 'Cookies',
+	},
+	en: {
+		'nav.inicio': 'Home',
+		'nav.vota': 'Vote',
+		'nav.info': 'Information',
+		'nav.archivo': 'Archive',
+		'nav.legal': 'Legal Notice',
+		'nav.privacidad': 'Privacy',
+		'nav.cookies': 'Cookies',
+	},
+} as const;
+
+export const routes = {
+	es: {
+		vota: 'vota',
+		info: 'info',
+		archivo: 'archivo',
+		'aviso-legal': 'aviso-legal',
+		privacidad: 'privacidad',
+		cookies: 'cookies',
+	},
+	en: {
+		vota: 'vote',
+		info: 'information',
+		archivo: 'archive',
+		'aviso-legal': 'legal-notice',
+		privacidad: 'privacy',
+		cookies: 'cookies',
+	},
+};
