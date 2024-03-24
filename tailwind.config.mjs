38e11a4138c98d1bc6906d0d28ed2eeddc794621
salema97 <salemavelasquez97@gmail.com>
2024-03-24 03:25:15 -0500

    Dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ module.exports = {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'
 	],
-	darkMode: 'media',
+	darkMode: 'class',
 	theme: {
 		extend: {}
 	},
