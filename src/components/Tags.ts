dccdb461336c3b5f3b86aebd9c78a111b17fccd6
salema97 <salemavelasquez97@gmail.com>
2024-03-26 02:00:59 -0500

    Customization of badge colors

---
+++ src/components/Tags.ts
@@ -0,0 +1,66 @@
+export const TAGS = {
+	ASTRO: {
+		name: 'Astro',
+		color: 'orange'
+	},
+	TAILWIND: {
+		name: 'Tailwind CSS',
+		color: 'blue'
+	},
+	CSHARP: {
+		name: 'C#',
+		color: 'blue'
+	},
+	HTML: {
+		name: 'HTML',
+		color: 'orange'
+	},
+	JAVASCRIPT: {
+		name: 'JavaScript',
+		color: 'yellow'
+	},
+	FIREBASE: {
+		name: 'Firebase',
+		color: 'yellow'
+	},
+	APIS: {
+		name: 'APIs',
+		color: 'yellow'
+	},
+	PHP: {
+		name: 'PHP',
+		color: 'blue'
+	},
+	JAVA: {
+		name: 'Java',
+		color: 'red'
+	},
+	SQL: {
+		name: 'SQL',
+		color: 'red'
+	},
+	POSTGRESQL: {
+		name: 'PostgreSQL',
+		color: 'blue'
+	},
+	CSS: {
+		name: 'CSS',
+		color: 'blue'
+	},
+	DESKTOP: {
+		name: 'Desarrollo de aplicaciones de escritorio',
+		color: 'blue'
+	},
+	WEB: {
+		name: 'Desarrollo de aplicaciones web',
+		color: 'blue'
+	},
+	MOVIL: {
+		name: 'Desarrollo de aplicaciones móviles',
+		color: 'blue'
+	},
+	FOXPRO: {
+		name: 'Visual FoxPro',
+		color: 'orange'
+	}
+}
