31a424b88d1477f17c724bcd6f8f9b1ede34cdc0
salema97 <salemavelasquez97@gmail.com>
2024-03-28 01:27:12 -0500

    Automatic image optimization, renaming and resizing of images

---
+++ scripts/transform-webp.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import sharp from 'sharp'
+
+function resizeAndConvertToWebP(
+	inputFolderPath: string,
+	outputFolderPath: string,
+	targetWidth: number,
+	quality: number
+): void {
+	fs.readdir(inputFolderPath, (err: NodeJS.ErrnoException | null, files: string[]) => {
+		if (err) {
+			console.error('Error al leer la carpeta de entrada:', err)
+			return
+		}
+
+		files.forEach((file) => {
+			const inputFilePath = path.join(inputFolderPath, file)
+			const outputFilePath = path.join(outputFolderPath, file.replace(/\.[^/.]+$/, '') + '.webp')
+
+			sharp(inputFilePath)
+				.resize({ width: targetWidth, withoutEnlargement: true })
+				.webp({ quality: quality })
+				.toFile(outputFilePath, (err: Error | null, info: sharp.OutputInfo) => {
+					if (err) {
+						console.error('Error al procesar el archivo:', err)
+					} else {
+						console.log('Imagen procesada exitosamente:', inputFilePath)
+					}
+				})
+		})
+	})
+}
+
+const inputFolderPath: string = './scripts/src/images/'
+const outputFolderPath: string = './public/images'
+const targetWidth: number = 854 // Ancho deseado, ajusta según tu necesidad
+
+resizeAndConvertToWebP(inputFolderPath, outputFolderPath, targetWidth, 60)
