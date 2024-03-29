a277a7f05e7fe56f9066101ab822b5db9d82ae53
salema97 <salemavelasquez97@gmail.com>
2024-03-29 03:21:40 -0500

    Add @astrojs/preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://salema97.github.io',
-	integrations: [tailwind()],
-	i18n: {
-		defaultLocale: 'es',
-		locales: ['es', 'en']
-	},
-	routing: {
-		prefixDefaultLocale: false
-	}
-})
+  site: 'https://salema97.github.io',
+  integrations: [tailwind(), preact()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en']
+  },
+  routing: {
+    prefixDefaultLocale: false
+  }
+});
